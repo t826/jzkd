e72a3e75638afc21c9235b233da69f5eea505c9d
t826 <1024653130@qq.com>
2021-05-10 17:36:30 +0800

    修改时间格式

--- response-cors.rkt
+++ response-cors.rkt
@@ -13,6 +13,7 @@
    #:headers (list (header #"Access-Control-Allow-Origin" #"*")
                    (header #"Access-Control-Allow-Headers" #"*")
                    (header #"Access-Control-Expose-Headers" #"X-Total-Count")
+                   (header #"Access-Control-Allow-Methods" #"*")
                    (header #"X-Total-Count"
                            (string->bytes/utf-8 (number->string total-count))))))
 
